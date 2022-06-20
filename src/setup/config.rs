e2a94702c6dfae1596403d953c5c69fedeba404a
Niklas <niklas@long.ch>
2022-06-20 15:19:15 +0200

    feat: implement initial node config write

--- src/setup/config.rs
+++ src/setup/config.rs
@@ -1,5 +1,6 @@
-use std::{ffi::OsString, fs, io, path::PathBuf};
+use std::{ffi::OsString, fmt::Write, fs, io, path::PathBuf};
 
+use anyhow::Result;
 use serde::Deserialize;
 
 // Ziggurat's configuration directory and file. Caches are written to this directory.
@@ -30,7 +31,7 @@ struct NodeMetaData {
 }
 
 impl NodeMetaData {
-    fn new(config_path: PathBuf) -> io::Result<NodeMetaData> {
+    fn new(config_path: PathBuf) -> Result<NodeMetaData> {
         // Read Ziggurat's configuration file.
         let path = config_path.join(CONFIG_FILE);
         let config_string = fs::read_to_string(path)?;
@@ -61,6 +62,8 @@ impl NodeMetaData {
 struct NodeConfig {
     /// The path of the cache directory of the node (and Ziggurat); this is `~/.ziggurat`.
     path: PathBuf,
+    /// The initial max number of peer connections to allow.
+    max_peers: usize,
 }
 
 impl NodeConfig {
@@ -71,10 +74,25 @@ impl NodeConfig {
                     io::Error::new(io::ErrorKind::NotFound, "couldn't find home directory")
                 })?
                 .join(CONFIG),
+            max_peers: 50,
         })
     }
 }
 
+struct RippledConfigFile;
+
+impl RippledConfigFile {
+    fn generate(config: &NodeConfig) -> Result<String> {
+        let mut config_str = String::new();
+
+        writeln!(&mut config_str, "[peers_max]")?;
+        writeln!(&mut config_str, "{}", config.max_peers)?;
+
+        Ok(config_str)
+    }
+}
+
+// TODO: split into separate file.
 struct Node {
     /// Fields to be written to the node's configuration file.
     config: NodeConfig,
@@ -84,11 +102,57 @@ struct Node {
 }
 
 impl Node {
-    fn new() -> io::Result<Self> {
+    fn new() -> Result<Self> {
         let config = NodeConfig::new()?;
         let meta = NodeMetaData::new(config.path.clone())?;
 
         Ok(Self { config, meta })
+    }
+
+    fn start(&self) -> Result<()> {
+        // cleanup any previous runs (node.stop won't always be reached e.g. test panics, or SIGINT)
+        self.cleanup()?;
+
+        self.generate_config_file()?;
+
+        // TODO: start the node process.
+
+        Ok(())
+    }
+
+    fn stop(&self) -> io::Result<()> {
+        // TODO: stop the node process and check for crash.
+
+        self.cleanup()
+    }
+
+    fn generate_config_file(&self) -> Result<()> {
+        let path = self.config.path.join(RIPPLED_CONFIG);
+        let content = RippledConfigFile::generate(&self.config)?;
+
+        fs::write(path, content)?;
+
+        Ok(())
+    }
+
+    fn cleanup(&self) -> io::Result<()> {
+        let path = self.config.path.join(RIPPLED_CONFIG);
+        match fs::remove_file(path) {
+            // File may not exist, so we supress the error.
+            Err(e) if e.kind() != std::io::ErrorKind::NotFound => Err(e),
+            _ => Ok(()),
+        }
+
+        // TODO: determine if any caches need to be cleanup up.
+    }
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        // We should avoid a panic.
+        if let Err(e) = self.stop() {
+            println!("Failed to stop the node: {}", e);
+        }
     }
 }
 
@@ -100,7 +164,9 @@ mod tests {
     fn config_works() {
         let node = Node::new().unwrap();
 
-        dbg!(node.config);
-        dbg!(node.meta);
+        // dbg!(node.config);
+        // dbg!(node.meta);
+
+        node.start().unwrap();
     }
 }
