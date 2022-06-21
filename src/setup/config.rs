99f743996c28192586ff3af8d7208cc3e0862c8a
Niklas <niklas@long.ch>
2022-06-21 16:10:59 +0200

    fix: initial rippled config

--- src/setup/config.rs
+++ src/setup/config.rs
@@ -9,8 +9,6 @@ const CONFIG_FILE: &str = "config.toml";
 
 // Rippled's configuration file name.
 pub const RIPPLED_CONFIG: &str = "rippled.cfg";
-// The path for the DB used by Rippled.
-const NUDB_PATH: &str = "db/nudb";
 
 /// Convenience struct for reading Ziggurat's configuration file.
 #[derive(Deserialize)]
@@ -91,22 +89,79 @@ impl RippledConfigFile {
     pub fn generate(config: &NodeConfig) -> Result<String> {
         let mut config_str = String::new();
 
-        // Peer configuration
+        // 1. Server
+
+        writeln!(&mut config_str, "[server]")?;
+        writeln!(&mut config_str, "port_rpc_admin_local")?;
+        writeln!(&mut config_str, "port_peer")?;
+
+        writeln!(&mut config_str, "[port_rpc_admin_local]")?;
+        writeln!(&mut config_str, "port = 5005")?;
+        writeln!(&mut config_str, "ip = 127.0.0.1")?;
+        writeln!(&mut config_str, "admin = 127.0.0.1")?;
+        writeln!(&mut config_str, "protocol = http")?;
+
+        writeln!(&mut config_str, "[port_peer]")?;
+        writeln!(&mut config_str, "port = 51235")?;
+        writeln!(&mut config_str, "ip = 127.0.0.1")?;
+        writeln!(&mut config_str, "protocol = peer")?;
+
+        // 2. Peer protocol
+
         writeln!(&mut config_str, "[peers_max]")?;
         writeln!(&mut config_str, "{}", config.max_peers)?;
-        // Separate sections with a newline.
-        writeln!(&mut config_str, "")?;
 
-        // DB configuration
+        writeln!(&mut config_str, "[sntp_servers]")?;
+        writeln!(&mut config_str, "time.windows.com")?;
+        writeln!(&mut config_str, "time.apple.com")?;
+        writeln!(&mut config_str, "time.nist.gov")?;
+        writeln!(&mut config_str, "pool.ntp.org")?;
+
+        // 3. Ripple protocol
+
+        writeln!(&mut config_str, "[validators_file]")?;
+        writeln!(&mut config_str, "validators.txt")?;
+
+        // 4. HTTPS client
+
+        writeln!(&mut config_str, "[ssl_verify]")?;
+        writeln!(&mut config_str, "0")?;
+
+        // 5. Reporting mode
+
+        // 6. Datababase
+
         writeln!(&mut config_str, "[node_db]")?;
         writeln!(&mut config_str, "type=NuDB")?;
         writeln!(
             &mut config_str,
             "{}",
-            config.path.join(NUDB_PATH).to_str().unwrap()
+            config.path.join("rippled/db/nudb").to_str().unwrap()
         )?;
         writeln!(&mut config_str, "online_delete=512")?;
         writeln!(&mut config_str, "advisory_delete=0")?;
+
+        writeln!(&mut config_str, "[database_path]")?;
+        writeln!(
+            &mut config_str,
+            "{}",
+            config.path.join("rippled/db").to_str().unwrap()
+        )?;
+
+        // 7. Diagnostics
+
+        writeln!(&mut config_str, "[debug_logfile]")?;
+        writeln!(
+            &mut config_str,
+            "{}",
+            config.path.join("rippled/debug.log").to_str().unwrap()
+        )?;
+
+        // 8. Voting
+
+        // 9. Misc settings
+
+        // 10. Example settings
 
         Ok(config_str)
     }
