8ddc731962c96db38fdaa90024ad671c579b3d6a
Niklas <niklas@long.ch>
2022-06-24 12:03:09 +0200

    test: initial handshake test (tentative)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 mod protocol;
 pub mod setup;
 mod tools;
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests/conformance/handshake.rs
@@ -0,0 +1,34 @@
+use pea2pea::Pea2Pea;
+
+use crate::{setup::node::Node, tools::synthetic_node::SyntheticNode};
+
+#[tokio::test]
+async fn handshake_when_node_receives_connection() {
+    crate::tools::synthetic_node::enable_tracing();
+
+    let mut node = Node::new().unwrap();
+    node.log_to_stdout(true).start().unwrap();
+
+    // Start synthetic node.
+    let node_config = pea2pea::Config {
+        listener_ip: Some("127.0.0.1".parse().unwrap()),
+        desired_listening_port: Some(12345),
+        allow_random_port: false,
+        ..Default::default()
+    };
+
+    // Wait for node to be fully started.
+    // TODO: replace with a connection from the node to signal readiness.
+    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+
+    let synth_node = SyntheticNode::new(node_config).await;
+    synth_node.node().connect(node.addr()).await.unwrap();
+
+    // This is only set post-handshake.
+    assert!(synth_node.node().is_connected(node.addr()));
+
+    // TODO: introduce the `wait_until!` macro.
+
+    // Gracefully shut down the nodes.
+    node.stop().unwrap();
+}
