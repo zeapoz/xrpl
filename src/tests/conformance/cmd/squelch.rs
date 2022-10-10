9e6f79df902634ca2317d8ba19144d8a19a0d6b4
Karlo <88337245+Rqnsom@users.noreply.github.com>
2022-10-10 12:10:40 +0200

    feat: add c016 for the mtSQUELCH message
     - Ensure squelching works correctly in a multi-node network environment.
       A synthetic node should be able to squelch distant validators.
     - SPEC.md updated accordingly

--- src/tests/conformance/cmd/squelch.rs
+++ src/tests/conformance/cmd/squelch.rs
@@ -28,7 +28,10 @@ use crate::{
         codecs::binary::{BinaryMessage, Payload},
         proto::{TmProposeSet, TmSquelch},
     },
-    setup::node::{Node, NodeType},
+    setup::{
+        constants::STATEFUL_NODES_COUNT,
+        node::{Node, NodeType},
+    },
     tools::{rpc::wait_for_state, synth_node::SyntheticNode},
 };
 
@@ -95,6 +98,117 @@ async fn c009_TM_SQUELCH_cannot_squelch_peer_ledger_proposals() {
 
     synth_node.shut_down().await;
     node.stop().expect("Unable to stop the stateful node");
+}
+
+#[tokio::test]
+#[allow(non_snake_case)]
+async fn c016_TM_SQUELCH_squelch_distant_validators() {
+    // ZG-CONFORMANCE-016
+
+    const DISTANT_NODES_CNT: usize = STATEFUL_NODES_COUNT - 1;
+
+    // We need to keep alive these temp directories for the whole duration of the test.
+    let target_dirs = (0..STATEFUL_NODES_COUNT)
+        .map(|_| TempDir::new().expect("Couldn't create a temporary directory"))
+        .collect::<Vec<TempDir>>();
+    let mut target = target_dirs.iter();
+
+    let mut builder = Node::builder();
+
+    // Create a stateful node that will be our synth node's only peer.
+    let mut peer_node = builder
+        .start(target.next().unwrap().path(), NodeType::Stateful)
+        .await
+        .expect("Unable to start the stateful node");
+
+    // Wait for correct state and account data.
+    wait_for_state(&peer_node.rpc_url(), "proposing".into()).await;
+
+    // Connect a synth node.
+    let mut synth_node = SyntheticNode::new(&Default::default()).await;
+    synth_node
+        .connect(peer_node.addr())
+        .await
+        .expect("Unable to connect");
+
+    // Get a validator public key from the only running node.
+    let peer_node_validator_key: Vec<u8> =
+        wait_for_validator_key_in_propose_msg(&mut synth_node).await;
+
+    // Prepare other nodes which are all mutually connected but not with the synth node.
+    let mut peer_addr_list = vec![peer_node.addr()];
+    let mut distant_nodes = vec![];
+    for _ in 0..DISTANT_NODES_CNT {
+        builder = builder
+            .log_to_stdout(false) // Explicit configuration until we really need to debug these nodes.
+            .initial_peers(peer_addr_list.clone());
+        let node = builder
+            .start(target.next().unwrap().path(), NodeType::Stateful)
+            .await
+            .expect("Unable to start the stateful node");
+
+        peer_addr_list.push(node.addr());
+        distant_nodes.push(node);
+    }
+
+    // Collect validation keys for distant nodes.
+    let mut distant_node_keys = vec![];
+    timeout(WAIT_MSG_TIMEOUT, async {
+        loop {
+            let node_pub_key = wait_for_validator_key_in_propose_msg(&mut synth_node).await;
+            if node_pub_key == peer_node_validator_key {
+                continue;
+            }
+
+            if !distant_node_keys.contains(&node_pub_key) {
+                distant_node_keys.push(node_pub_key);
+                if distant_node_keys.len() == DISTANT_NODES_CNT {
+                    break;
+                }
+            }
+        }
+    })
+    .await
+    .expect("TmProposeLedger not received in time");
+
+    // Squelch distant nodes.
+    for key in distant_node_keys.iter() {
+        let msg = Payload::TmSquelch(TmSquelch {
+            squelch: true,
+            validator_pub_key: key.clone(),
+            squelch_duration: Some(SQUELCH_DURATION_SECS),
+        });
+        synth_node.unicast(peer_node.addr(), msg).unwrap();
+    }
+
+    // Ensure all incoming TmProposeLedger messages are handled before nodes process the squelch message.
+    sleep(HANDLE_REMAINING_PROPOSE_MSGS).await;
+
+    // Verify we are not receiving TmProposeLedger messages from distant nodes.
+    let expect_timeout_err_for_squelched_nodes = timeout(WAIT_MSG_TIMEOUT, async {
+        loop {
+            if let (
+                _,
+                BinaryMessage {
+                    payload: Payload::TmProposeLedger(TmProposeSet { node_pub_key, .. }),
+                    ..
+                },
+            ) = synth_node.recv_message().await
+            {
+                if distant_node_keys.contains(&node_pub_key) {
+                    panic!("It shouldn't be possible to receive proposing ledgers from squelched nodes.");
+                }
+            }
+        }
+    }).await;
+
+    assert!(expect_timeout_err_for_squelched_nodes.is_err());
+
+    synth_node.shut_down().await;
+    peer_node.stop().expect("Unable to stop the stateful node");
+    for mut node in distant_nodes {
+        node.stop().expect("Unable to stop the stateful node");
+    }
 }
 
 async fn wait_for_validator_key_in_propose_msg(synth_node: &mut SyntheticNode) -> Vec<u8> {
