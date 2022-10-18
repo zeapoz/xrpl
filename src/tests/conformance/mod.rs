f77c656731dd970a75f6c426a9d6743e3f7d4cdc
Łukasz Wojtów <lukasz.wojtow@users.noreply.github.com>
2022-10-18 12:54:14 +0200

    Refactor: Extract code checking for relayed message after a transaction

--- src/tests/conformance/mod.rs
+++ src/tests/conformance/mod.rs
@@ -2,8 +2,17 @@ use tempfile::TempDir;
 
 use crate::{
     protocol::codecs::binary::BinaryMessage,
-    setup::node::{Node, NodeType},
-    tools::{config::TestConfig, synth_node::SyntheticNode},
+    setup::{
+        constants::TESTNET_READY_TIMEOUT,
+        node::{Node, NodeType},
+        testnet::TestNet,
+    },
+    tools::{
+        config::TestConfig,
+        constants::GENESIS_ACCOUNT,
+        rpc::{submit_transaction, wait_for_account_data},
+        synth_node::SyntheticNode,
+    },
 };
 
 mod cmd;
@@ -20,6 +29,9 @@ pub const PUBLIC_KEY_TYPES: &[u8] = &[
 ];
 
 pub const PUBLIC_KEY_LENGTH: usize = 33; // A key consists of 1 magic byte for key type and 32 bytes for encryption bits.
+
+// A transaction blob representing a signed transaction. Extracted by executing `tools/transfer.py` and listening with `tcpdump -A -i lo dst port 5005 or src port 5005`.
+pub const TRANSACTION_BLOB: &str = "12000022000000002400000001201B0000001E61400000012A05F20068400000000000000A73210330E7FC9D56BB25D6893BA3F317AE5BCF33B3291BD63DB32654A313222F7FD020744630440220297389244D36AF12115296F409C446D9A5D808880DC7FF323AA207ED529CE6C802207AAC5D2A96CB102CBDE85D2A4BA814253CA133AC9277041CAE2E1A349FB233FF8114B5F762798A53D543A014CAF8B297CFF8F2F937E883149193D6AED0CBBC25790ADE05D020C9C6D9201DCF";
 
 async fn perform_response_test(
     config: TestConfig,
@@ -48,4 +60,51 @@ async fn perform_response_test(
     // Shutdown both nodes
     synth_node.shut_down().await;
     node.stop().unwrap();
+}
+
+/// Performs a check for the required message after a new transaction in the testnet.
+/// Scenario:
+/// 1. Start a testnet and wait for 'ready' status.
+/// 2. Connect a SyntheticNode to the second rippled node in the testnet.
+/// 3. Submit a transaction via RPC call to the first rippled node in the testnet.
+/// 4. Assert that the SyntheticNode received the required message.
+pub async fn perform_testnet_transaction_check(check: &dyn Fn(&BinaryMessage) -> bool) {
+    const NODE_IDS: [usize; 2] = [0, 1];
+
+    // Start a testnet.
+    let mut testnet = TestNet::new().unwrap();
+    testnet.start().await.unwrap();
+    wait_for_account_data(
+        &testnet.running[NODE_IDS[0]].rpc_url(),
+        GENESIS_ACCOUNT,
+        TESTNET_READY_TIMEOUT,
+    )
+    .await
+    .expect("Unable to get the account data.");
+
+    // Start a synthetic node and connect to the second node in the testnet.
+    let mut synth_node = SyntheticNode::new(&Default::default()).await;
+    synth_node
+        .connect(testnet.running[NODE_IDS[1]].addr())
+        .await
+        .expect("Unable to connect to the second node");
+
+    // Submit a transaction to the first node via RPC.
+    let transaction = submit_transaction(
+        &testnet.running[NODE_IDS[0]].rpc_url(),
+        TRANSACTION_BLOB.into(),
+        false,
+    )
+    .await
+    .expect("Unable to submit the transaction.");
+    assert!(transaction.result.accepted);
+    assert!(transaction.result.applied);
+    assert!(transaction.result.broadcast);
+
+    // Ensure that the synthetic node connected to the second node received the required message.
+    assert!(synth_node.expect_message(&check).await);
+
+    // Shutdown.
+    testnet.stop().await.expect("Unable to stop the testnet.");
+    synth_node.shut_down().await;
 }
